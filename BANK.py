6ca55ea44007d349c0dba15d1b6aef1a725d83de
moosekaka <sweel@uci.edu>
2016-06-06 13:55:14 -0700

    bank

--- BANK.py
+++ BANK.py
@@ -8,8 +8,17 @@ Created on Tue Sep 22 12:29:53 2015
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from seaborn import xkcd_palette as scolor
+# pylint: disable=C0103
+
+
+def rgbcol(colorname):
+    """
+    returns RGB float values based on xkcd color name input string
+    """
+    return scolor([colorname])[0]
+
 plt.style.use('ggplot')
-#pd.set_option('display.mpl_style', 'default')
 plt.close('all')
 
 # =============================================================================
@@ -43,7 +52,7 @@ df1 = pd.pivot_table(spend,
                      values='Amount',
                      index='Date',
                      aggfunc='sum')
-MthlySpd = df1.resample('M', how='sum')
+MthlySpd = df1.resample('M').sum()
 
 
 # =============================================================================
@@ -53,15 +62,16 @@ spend['Category'] = ''
 
 spend.loc[spend.Desc.str.contains('ATM'), ['Category']] = 'ATMcash'
 
-pattern = r'COF|PEE|STARBU|ARAMARK UCI'
+pattern = r'COF|PEE|STARBU|ARAMARK UCI|BED'
 spend.loc[spend.Desc.str.contains(pattern), ['Category']] = 'Coffee'
 
 spend.loc[spend.Desc.str.contains(
-    'BCD|BOIL|Boil|YOGURT|MEET FRESH|BEER'), ['Category']] = 'AsianFood'
+    'BCD|BOIL|Boil|YOGURT|MEET FRESH\
+    |MITSUWA-BEER'), ['Category']] = 'AsianFood'
 
-pattern = 'JACK|^CHICK|MCD|^DEL|^IN\
+pattern = 'JACK|^CHICK|MCD|DEL TACO|IN-N-OUT\
 |TACO BELL|SUBWAY|PANERA|^EL P|CHIPOTLE|BLAZE|HABIT|IKEA R\
-|SEAFOOD C|DENNY|IHOP|POPEY|SLAPFISH|PHO SAIGON|Flame|FLAME'
+|SEAFOOD C|DENNY|IHOP|POPEY|SLAPFISH|PHO SAIGON|Flame|FLAME|KFC|Pizza 90'
 spend.loc[spend.Desc.str.contains(pattern), ['Category']] = 'FastFood'
 
 pattern = 'J J BAKE|NORM|TENDER GR|URBAN SEOUL|KAISEN|KULA|BLAKES\
@@ -70,27 +80,29 @@ spend.loc[spend.Desc.str.contains(pattern), ['Category']] = 'Restaurants'
 
 spend.loc[spend.Desc.str.contains('LAZY'), ['Category']] = 'Lazy'
 
-spend.loc[spend.Desc.str.contains('USA|A[Rr][Cc]'), ['Category']] = 'Petrol'
+spend.loc[spend.Desc.str.contains('USA|A[Rr][Cc]'), ['Category']] = 'Gas'
 
 pattern = '99 RAN|PAVILIONS|VONS|ALBERT\
-|RALP|TARG|^WAL|WM|^TRADER|MITSUWA MAR|WHOLESOME|H MART|SPROUTS|BED'
+|RALP|TARG|^WAL|WM|^TRADER|MITSUWA MAR|WHOLESOME|H MART|SPROUTS\
+|MITSUWA MRKTPLACE|WHOLEFDS'
 spend.loc[spend.Desc.str.contains(pattern), ['Category']] = 'Market'
 
 pattern = 'AMC|FANDANGO|CINEMA|EDWARD|Kindle|MICRO CENTER|FRY\'S\
-|DSW METRO POINT|UCI CAMPUS RECREATION'
-spend.loc[spend.Desc.str.contains(pattern), ['Category']] = 'Extra'
+|DSW METRO POINT|UCI CAMPUS RECREATION|IRVINE-BOOKSTORE-HILL|T-MOBI|SAMY'
+spend.loc[spend.Desc.str.contains(pattern), ['Category']] = '_\
+Books, Elec. etc.'
 
 pattern = 'FARMERS IN|AM SOC|USPS 053711025|U-HAUL\
-|UCI TRANSPO|VISTA DEL|IMAGE D|COINBASE|EVA|UMI|PRIME TIME|VR|HARBOR J\
-|T-MOBI|SAMY|BIOMEDICAL ENGINEERING|AUTOZONE|PERFORMANCE BIKE SHOP\
-|STATE OF CALIF DMV'
-spend.loc[spend.Desc.str.contains(pattern), ['Category']] = 'Misc'
+|UCI TRANSPO|VISTA DEL|IMAGE D|COINBASE|EVA|UMI|PRIME TIME|VR|HARBOR J \
+|BIOMEDICAL ENGINEERING|AUTOZONE|PERFORMANCE BIKE SHOP\
+|STATE OF CALIF DMV|OPTOMETRIC|TOWING'
+spend.loc[spend.Desc.str.contains(pattern), ['Category']] = '_One-offs, Fees'
 
 pattern = 'UCI PARK|GOOGLE *|UCI TRANS'
-spend.loc[spend.Desc.str.contains(pattern), ['Category']] = 'PhonePark'
+spend.loc[spend.Desc.str.contains(pattern), ['Category']] = 'Phone, Parking'
 
 pattern = 'AMZ  STORECARD|PAYPAL|AMAZON'
-spend.loc[spend.Desc.str.contains(pattern), ['Category']] = 'Deferred'
+spend.loc[spend.Desc.str.contains(pattern), ['Category']] = '_Deferred'
 
 
 #   find cashback items, exclude cashback items from original set
@@ -116,58 +128,76 @@ netgro = pd.DataFrame(  # grocery items minus cashback
 
 #    concat back the excluded , new groups
 total = pd.concat([spend, cashback, netgro], ignore_index=True)
-
+uncat = total[total.Category.apply(lambda x: x == '')]
+total = total.set_value(uncat.index, 'Category', 'Uncat')
 # =============================================================================
 #    Pivot to group by cat and months and plot
 # =============================================================================
-date_range = '2015-9'
-dollar_lim = 2500
+date_range = '2015-10'
+dollar_lim = 2000
+mth_lim = 250
 MthlySpd = - MthlySpd.ix[date_range:]
 df = pd.pivot_table(total,
                     index='Date',
                     columns='Category',
                     values='Amount',
                     aggfunc='sum')
-byMonth = df.resample('M', how='sum')
-cols = [u'ATMcash', u'AsianFood', u'Coffee',
-        u'Extra', u'FastFood', u'Lazy', u'Market',
-        u'Petrol', u'Restaurants', u'PhonePark', u'Misc', u'Deferred']
-Filt = -byMonth.ix[date_range:, 1:]
+byMonth = df.resample('M').sum()
+cols = sorted([c for c in pd.unique(total.Category.values) if c!='Uncat'])
+Filt = -byMonth.ix[date_range:, :]
 
 #    Plot
-fig = plt.plot(figsize=(11, 8.5))
-ax = plt.subplot(111)
+#fig = plt.plot(figsize=(11, 8.5))
+f1, ax = plt.subplots()
 ax.set_ylim(0, MthlySpd.max() + dollar_lim)
-ax.set_axis_bgcolor('#C0C0C0')
+ax.set_axis_bgcolor(rgbcol('grey'))
 ax.set_title('Stacked Monthly Expenditure')
-Filt.ix[:, cols].plot(kind='Area',
+Filt.ix[:, cols].plot(kind='area',
                       colormap=plt.cm.Paired,
-                      sort_columns=True,
                       ax=ax)
-
 MthlySpd.plot(ax=ax)
 ax.set_ylim(0, dollar_lim)
-fig2 = plt.plot(figsize=(11, 8.5))
-ax2 = plt.subplot(111)
-ax2 = Filt.ix[:, cols].plot(kind='bar', colormap=plt.cm.Paired)
-ax2.set_axis_bgcolor('#808080')
+ax.axhline(1000, c=rgbcol('hot pink'))
+
+#fig2 = plt.plot(figsize=(11, 8.5))
+f2, ax2 = plt.subplots()
+ax2 = Filt.ix[:, cols].plot(kind='bar',
+                            colormap=plt.cm.Paired,
+                            ax=ax2)
+ax2.set_axis_bgcolor(rgbcol('grey'))
 ax2.set_xticklabels(
     [dt.strftime('%b %y')
      for dt in Filt.index.to_pydatetime()])
-ax2.set_ylim(0, 250)
+ax2.set_ylim(0, mth_lim)
 plt.xticks(rotation=0)
 
 #
-x = total[(total.Date > '2015-01-30')]
+x = total[(total.Date > '2015-01-30')].reset_index(drop=True)
 y = x.groupby(['Date', 'Category']).sum()
+y1 = y.reset_index()
 ycount = x.groupby(['Date', 'Category']).count()
 #
-z = y.unstack(level=1).resample('M', how='sum')
-zcount = ycount['Amount'].unstack(level=1).resample('M', how=sum)
-ztot = z.sum(axis=1)
-yf = y.query('Category == "Lazy"')
-yf1 = yf.reset_index(level=1)
-print yf1.resample('M', how=len).Category
-print z
-print '%s\n\n\n%s\n' % (z, zcount)
-uncat = total[(total.Date > '2015-06-30') & (total.Category == '')]
+z = y.unstack(level=1).resample('M').sum()
+z_count = y.unstack(level=1).resample('M').count()
+z.columns = z.columns.droplevel(0)
+z_count.columns = z_count.columns.droplevel(0)
+z['Total'] = z.sum(axis=1)
+
+# ============================================================================
+# Number of visits to xxx
+# ============================================================================
+print '\n{}\nNumber of occur. of \n{}\n{}\n'.format('='*79, '='*79, z_count)
+
+# ============================================================================
+# Category spending
+# ============================================================================
+print '\n{}\nSpending by category to \n{}\n{}\n'.format('='*79, '='*79, z)
+
+#==============================================================================
+# Write out to file
+#==============================================================================
+with open('total.txt', 'w') as output:
+    output.write('\n{}\nNumber of occur.\
+    of \n{}\n{}\n'.format('='*79, '='*79, z_count))
+    output.write('\n{}\nSpending by category\
+    of \n{}\n{}\n'.format('='*79, '='*79, z))

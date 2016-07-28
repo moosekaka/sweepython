a23bd04852277c4dc7e2393c3d71ed4b061dab95
moosekaka <sweel@uci.edu>
2016-07-28 10:29:48 -0700

    after lab meeting

--- mombud/vtkhandler.py
+++ mombud/vtkhandler.py
@@ -5,9 +5,11 @@ Module for plots of analysis of mother bud function in budding yeast
 """
 import sys
 import os.path as op
+import cPickle as pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from wrappers import UsageError
 import mombud.mungedf as munge
 import mombud.functions.vtk_mbplots as mbfuncs
@@ -23,6 +25,16 @@ gfp_plot_vars =  ['DY_abs_mean_mom',
                   'DY_abs_mean_bud',
                   'DY_abs_cell_mean']
 mombud_dy_vars = ['DY_median_mom', 'DY_median_bud']
+
+
+def ranger(lst, mult):
+    """
+    return array for y-ticks or x-ticks with multiples of `mult`
+    from limits in `lst`
+    """
+    yt = [i - i % mult for i in lst]
+    return np.arange(yt[0], yt[1], mult)
+
 
 def main():
     """
@@ -42,40 +54,41 @@ def main():
                     'binsvolmom': np.array([0, 30, 40, 80.]),
                     }
         outputargs = munge.postprocess_df(**inp_args)
-        df = outputargs.get('data')
+#        df = outputargs.get('data')
+
+        with open('df_mombud_filtered.pkl', 'rb') as inpt:
+            df = pickle.load(inpt)
         dfmom = outputargs.get('dfmom')
-        dfbud= outputargs.get('dfbud')
-#==============================================================================
+        dfbud = outputargs.get('dfbud')
+        dfmom = dfmom[dfmom.index.isin(df.index)]
+        dfbud = dfbud[dfbud.index.isin(df.index)]
+# =============================================================================
 # Data long form
-#==============================================================================
+# =============================================================================
         frac = pd.melt(df, id_vars=['media'],
                        value_vars=['frac'])
-        frac_cellcycle =  df[['media','bin_budprog', 'frac']].reset_index(drop=True)
+        frac_cellcycle = (df[['media','bin_budprog', 'frac']]
+                          .reset_index(drop=True))
         mb_dy = pd.melt(df, id_vars=['media', 'date'],
                         value_vars=mombud_dy_vars)
         mb_gfp = pd.melt(df, id_vars=['media', 'date'],
-                        value_vars=gfp_plot_vars)
+                         value_vars=gfp_plot_vars)
         diameters = pd.melt(df, id_vars='media',
                             value_vars=['bud_diameter', 'mom_diameter'])
-        size = pd.melt(df.loc[:, ['media','budvol', 'momvol']],'media')
+        size = pd.melt(df.loc[:, ['media', 'budvol', 'momvol']],'media')
         momdy = pd.melt(dfmom,
                         id_vars=['media', 'binvol'],
                         var_name='mom axis position',
                         value_name=r'$\Delta\Psi$ scaled',
                         value_vars=outputargs['mbax'].tolist())
 #        momdy = momdy.dropna()
-        buddy= pd.melt(dfbud,
-                       id_vars=['media', 'binvol'],
-                       var_name='bud axis position',
-                       value_name=r'$\Delta\Psi$ scaled',
-                       value_vars=outputargs['mbax'].tolist())
+        buddy = pd.melt(dfbud,
+                        id_vars=['media', 'binvol'],
+                        var_name='bud axis position',
+                        value_name=r'$\Delta\Psi$ scaled',
+                        value_vars=outputargs['mbax'].tolist())
 #        buddy = buddy.dropna()
-    # mutant only subset
-        mutants = (mb_dy[mb_dy['media']
-                   .isin(['NUM1', 'MFB1', 'YPT11', 'WT'])]
-                   .reset_index(drop=True))
-        group = mutants.groupby('date')
-#==============================================================================
+
         outkws1 = dict(default_ylims=[0.05, 0.95],
                        labeller=labelNormal, col_order=COL_ODR)
 
@@ -90,15 +103,6 @@ def main():
         plva.plt(data=mb_gfp, **seta)
         plva.turn_off_legend()
         plva.save_figure(op.join(savefolder, 'gfp_mombud.png'))
-
-        set0 = dict(x='media', y='value',
-                    hue='variable', group_key='media',
-                    title='mombud', ylim=(0, 1.),
-                    )
-        plv = plviol(**outkws1)
-        plv.plt(data=group.get_group('032016'), **set0)
-        plv.turn_off_legend()
-        plv.save_figure(op.join(savefolder, 'mutantsDY_mombud.png'))
 
         set1 = dict(x='media', y='value',
                     hue='variable', group_key='media',
@@ -174,6 +178,25 @@ def main():
         plv8.plt(data=frac_cellcycle,
                  mapargs=['bin_budprog', 'frac'], **set8)
 
+        # facetted buds plot
+        bigbinsbud = pd.melt(dfbud,
+                             id_vars=['media', 'binvol'],
+                             var_name='bud axis position',
+                             value_vars=dfbud.columns[:5].tolist())
+        bigbinsbud = bigbinsbud.dropna()
+        with sns.plotting_context('talk', font_scale=.85):
+            ylims2 = (bigbinsbud['value']
+                      .quantile([0.025, 0.975]).tolist())
+            yt = ranger(ylims2, 0.25)
+            m0 = sns.FacetGrid(bigbinsbud,
+                               row='media',
+                               col="binvol",
+                               hue='media',
+                               row_order=COL_ODR,
+                               )
+            m0 = (m0.map(sns.pointplot, 'bud axis position', 'value')
+                  .set(yticks=yt, ylim=tuple(ylims2)))
+
         print "Finished plotting!"
         return 0
 
@@ -181,5 +204,5 @@ def main():
         print e
         return 1
 
-if __name__=='__main__':
+if __name__ == '__main__':
     sys.exit(main())

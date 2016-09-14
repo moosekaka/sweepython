cef918f0822c8ab8dd386dd32c4ab3e8346ae9f6
moosekaka <sweel@uci.edu>
2016-09-13 23:15:58 -0700

    slides for meet

--- mombud/vtkhandler2.py
+++ mombud/vtkhandler2.py
@@ -3,25 +3,24 @@
 Created on Sat Jun 11 16:10:16 2016
 Module for plots of analysis of mother bud function in budding yeast
 """
-import sys
 import os.path as op
 import cPickle as pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from wrappers import UsageError
 import mombud.mungedf as munge
 import mombud.functions.vtk_mbplots as mbfuncs
 
 labelhandler, plviol, plbox, plfacet = (mbfuncs.labelhandler,
                                         mbfuncs.plviol,
-                                        mbfuncs.plbox,
+                                        mbfuncs.plbox2,
                                         mbfuncs.plfacet)
-COL_ODR = munge.COL_ODR
-COL_ODR = ['MFB1', 'NUM1', 'NORM. NUM1', 'YPT11', 'WT_COMBINED', 'YPL', 'YPR']
+COL_ODR = ['MFB1', 'DEFECT. NUM1', 'NORM. NUM1', 'NUM1', 'YPT11',
+           'WT', 'YPE', 'WT_COMBINED',  'YPL', 'YPR', ]
+
 HUE_ODR = munge.HUE_ODR
-savefolder = munge.datadir
+savefolder = r"C:\Users\sweel_Rafelski\Dropbox\SusanneSweeShared\091316"
 mombud_dy_vars = ['DY_median_mom', 'DY_median_bud']
 
 date_order = ['042515', '042715', '052315','032016', '071016', '071116']
@@ -40,21 +39,8 @@ normal_num1 = ['NUM1_032016_003_RFPstack_021',
                'NUM1_071016_032_RFPstack_115',
                'NUM1_071016_032_RFPstack_116']
 
-def ranger(lst, mult):
-    """
-    return array for y-ticks or x-tic+ks with multiples of `mult`
-    from limits in `lst`
-    """
-    yt = [i - i % mult for i in lst]
-    return np.arange(yt[0], yt[1], mult)
 
-
-#def main():
-#    """
-#    Main
-#    """
 plt.close('all')
-#try:
 labelFacet = labelhandler('facet')
 labelNormal = labelhandler()
 labelRowFacet = labelhandler('rowfacet')
@@ -67,43 +53,51 @@ inp_args = {'inpdatpath': 'celldata.pkl',
             'binsvolmom': np.array([0, 30, 40, 80.]),
             }
 outputargs = munge.postprocess_df(**inp_args)
-#        df = outputargs.get('data')
 
 with open('df_mombud_filtered.pkl', 'rb') as inpt:
     df = pickle.load(inpt)
 
-df['media_combined'] = df.media
-df.loc[df.media_combined.isin(['YPE', 'WT']), 'media_combined'] = 'WT_COMBINED'
-df.loc[df.index.isin(normal_num1), 'media_combined'] = 'NORM. NUM1'
+df['media2'] = np.nan
+df.loc[df.media.isin(['YPE', 'WT']), 'media2'] = 'WT_COMBINED'
+df.loc[df.index.isin(normal_num1), 'media2'] = 'NORM. NUM1'
+num1_all = df.loc[df.media == 'NUM1'].index
+num1_mutant = num1_all[~num1_all.isin(normal_num1)]
+df.loc[num1_mutant, 'media2'] = 'DEFECT. NUM1'
 
 dfmom = outputargs.get('dfmom')
+dfmom = dfmom[dfmom.index.isin(df.index)].reset_index()
 dfbud = outputargs.get('dfbud')
-dfmom = dfmom[dfmom.index.isin(df.index)]
-dfbud = dfbud[dfbud.index.isin(df.index)]
+dfbud = dfbud[dfbud.index.isin(df.index)].reset_index()
+
+dfmom['media2'] = np.nan
+dfmom.loc[dfmom.media.isin(['YPE', 'WT']), 'media2'] = 'WT_COMBINED'
+dfmom.loc[dfmom.name.isin(normal_num1), 'media2'] = 'NORM. NUM1'
+num1_all = dfmom.loc[dfmom.media == 'NUM1'].name
+num1_mutant = num1_all[~num1_all.isin(normal_num1)]
+dfmom.loc[dfmom.name.isin(num1_mutant), 'media2'] = 'DEFECT. NUM1'
+
+dfbud['media2'] = np.nan
+dfbud.loc[dfbud.media.isin(['YPE', 'WT']), 'media2'] = 'WT_COMBINED'
+dfbud.loc[dfbud.name.isin(normal_num1), 'media2'] = 'NORM. NUM1'
+num1_all = dfbud.loc[dfbud.media == 'NUM1'].name
+num1_mutant = num1_all[~num1_all.isin(normal_num1)]
+dfbud.loc[dfbud.name.isin(num1_mutant), 'media2'] = 'DEFECT. NUM1'
 # =============================================================================
 # Data long form
 # =============================================================================
-frac = pd.melt(df, id_vars=['media_combined'],
+frac = pd.melt(df, id_vars=['media'],
                value_vars=['frac'])
+frac1 = pd.melt(df, id_vars=['media2'],
+               value_vars=['frac']).dropna()
+frac1.rename(columns = {'media2':'media'}, inplace=True)
+frac = frac.append(frac1)
 
-mb_dy = pd.melt(df, id_vars=['media', 'date'],
+mb_dy = pd.melt(df, id_vars=['media'],
                 value_vars=mombud_dy_vars)
-
-
-momdy = pd.melt(dfmom,
-                id_vars=['media', 'binvol'],
-                var_name='mom axis position',
-                value_name=r'$\Delta\Psi$ scaled',
-                value_vars=outputargs['mbax'].tolist())
-
-buddy = pd.melt(dfbud,
-                id_vars=['media', 'binvol'],
-                var_name='bud axis position',
-                value_name=r'$\Delta\Psi$ scaled',
-                value_vars=outputargs['mbax'].tolist())
-
-outkws_wt = dict(default_ylims=[0.05, 0.95],
-                 labeller=labelNormal, col_order=date_order)
+mb_dy1 = pd.melt(df, id_vars=['media2'],
+                value_vars=mombud_dy_vars).dropna()
+mb_dy1.rename(columns = {'media2':'media'}, inplace=True)
+mb_dy = mb_dy.append(mb_dy1)
 
 outkws1 = dict(default_ylims=[0.05, 0.95],
                labeller=labelNormal, col_order=COL_ODR)
@@ -111,33 +105,63 @@ outkws1 = dict(default_ylims=[0.05, 0.95],
 outkws2 = dict(default_ylims=[0.15, 0.9],
                labeller=labelFacet, col_order=COL_ODR)
 
-set_wt = dict(x='date', y='value',
-            hue='variable', group_key='date',
-            title='DY by date', ylim=(0, 1.),
-            )
-plv_wt = plviol(**outkws_wt)
-plv_wt.plt(data=mb_dy, **set_wt)
-plv_wt.save_figure(op.join(savefolder, 'wt_violin_mombud.png'))
-
-
+ # FracDY plot
 set1 = dict(x='media', y='value',
             hue='variable', group_key='media',
             title='mombud', ylim=(0, 1.),
             )
+
 plv1 = plviol(**outkws1)
 plv1.plt(data=mb_dy, **set1)
 plv1.turn_off_legend()
 plv1.save_figure(op.join(savefolder, 'violin_mombud.png'))
 
-set2 = dict(x='media_combined', y='value', ylim='auto',
-            hue='media_combined', group_key='media_combined', inner=None,
-            title='fracDY')
-plv2 = plbox(**outkws1)
-plv2.plt(data=frac, **set2)
-plv2.turn_off_legend()
-plv2.save_figure(op.join(savefolder, 'fracDY.png'))
+ # mombudDY plot
+set2 = dict(x='media', y='value', data=frac, width=.75,
+             order=COL_ODR, notch=True, bootstrap=100000)
+g = sns.boxplot(**set2)
+g.set(ylim=(0, 3.5))
+plt.savefig(op.join(savefolder, 'fracDY.png'))
 
-set5 = dict(col_wrap=4, col='media', hue='media',
+set2v = dict(x='media', y='value',
+           group_key='media', inner=None,
+            title='fracDY', ylim='auto')
+plv2 = plviol(**outkws1)
+plv2.plt(data=frac, **set2v)
+plv2.turn_off_legend()
+#plv2.save_figure(op.join(savefolder, 'test.png'))
+
+ # mom and bud cell axis DY plots
+momdy = pd.melt(dfmom,
+                id_vars=['media', 'binvol'],
+                var_name='mom axis position',
+                value_name=r'$\Delta\Psi$ scaled',
+                value_vars=outputargs['mbax'].tolist())
+
+momdy1 = pd.melt(dfmom,
+                 id_vars=['media2', 'binvol'],
+                 var_name='mom axis position',
+                 value_name=r'$\Delta\Psi$ scaled',
+                 value_vars=outputargs['mbax'].tolist()).dropna()
+momdy1.rename(columns = {'media2':'media'}, inplace=True)
+momdy = momdy.append(momdy1)
+
+buddy = pd.melt(dfbud,
+                id_vars=['media', 'binvol'],
+                var_name='bud axis position',
+                value_name=r'$\Delta\Psi$ scaled',
+                value_vars=outputargs['mbax'].tolist())
+
+buddy1 = pd.melt(dfbud,
+                 id_vars=['media2', 'binvol'],
+                 var_name='bud axis position',
+                 value_name=r'$\Delta\Psi$ scaled',
+                 value_vars=outputargs['mbax'].tolist()).dropna()
+buddy1.rename(columns = {'media2':'media'}, inplace=True)
+buddy = buddy.append(buddy1)
+
+
+set5 = dict(col_wrap=5, col='media', hue='media',
             sharex=True, sharey=True, col_order=COL_ODR,
             ylim=(0.0, 1.),
             )
@@ -147,7 +171,7 @@ plv5.plt(data=momdy,
          **set5)
 plv5.save_figure(op.join(savefolder, 'momDY.png'))
 
-set6 = dict(col_wrap=4, col='media', hue='media',
+set6 = dict(col_wrap=5, col='media', hue='media',
             sharex=True, sharey=True, col_order=COL_ODR,
             ylim=(0.0, 1.0),
             )
@@ -156,31 +180,3 @@ plv6.plt(data=buddy,
          mapargs=['bud axis position', r'$\Delta\Psi$ scaled'],
          **set6)
 plv6.save_figure(op.join(savefolder, 'budDY.png'))
-
-set7 = dict(x='bin_budprog', y='frac',
-            hue='media',
-            title=u"Δψ vs bud progression\n ", ylim=(0., 3.),
-            setargs=dict(xlabel="bud progression",
-                         ylabel=u"Δψ bud/Δψ mom",)
-            )
-
-# facetted buds plot
-bigbinsbud = pd.melt(dfbud,
-                     id_vars=['media', 'binvol'],
-                     var_name='bud axis position',
-                     value_vars=dfbud.columns[:5].tolist())
-bigbinsbud = bigbinsbud.dropna()
-
-with sns.plotting_context('talk', font_scale=.85):
-    ylims2 = (bigbinsbud['value']
-              .quantile([0.025, 0.975]).tolist())
-    yt = ranger(ylims2, 0.25)
-    m0 = sns.FacetGrid(bigbinsbud,
-                       row='media',
-                       col="binvol",
-                       hue='media',
-                       row_order=COL_ODR,
-                       )
-    m0 = (m0.map(sns.pointplot, 'bud axis position', 'value')
-          .set(yticks=yt, ylim=tuple(ylims2)))
-

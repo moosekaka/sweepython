b4557de691e44bda6d3a84ffaf977d78b01a533e
moosekaka <sweel@uci.edu>
2016-12-07 14:54:24 -0800

    set colors

--- mombud/vtkhandler2.py
+++ mombud/vtkhandler2.py
@@ -5,6 +5,8 @@ Module for plots of analysis of mother bud function in budding yeast
 """
 import os
 import os.path as op
+import operator
+from collections import OrderedDict
 import cPickle as pickle
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,9 +23,9 @@ labelhandler, plviol, plbox, plfacet = (mbfuncs.labelhandler,
                                         mbfuncs.plbox,
                                         mbfuncs.plfacet)
 
-COL_ODRfra = [u'ΔMFB1', u'ΔNUM1', u'ΔYPT11', u'WT_YPE', u'WT_YPL', u'WT_YPR']
-COL_ODR = [u'WT_YPE', u'WT_YPL', u'WT_YPR', u'ΔMFB1', u'ΔNUM1', u'ΔYPT11']
-HUE_ODR = [u'ΔMFB1', u'ΔNUM1', u'ΔYPT11', u'WT_YPE', u'WT_YPL', u'WT_YPR']
+plt_labs = [u'Eth. + Glyc.', u'Lactate',  u'Raffinose',
+            u'ΔMFB1', u'ΔNUM1', u'ΔYPT11']
+colorblind = ["#0072B2", "#009E73", "#D55E00", "#CC79A7", "#F0E442", "#56B4E9"]
 savefolder = op.expanduser(os.sep.join(['~', 'Dropbox', 'SusanneSweeShared',
                                         'figures_for_mutants2']))
 mombud_dy_vars = ['DY_median_mom', 'DY_median_bud']
@@ -40,7 +42,6 @@ with open(op.join('cellVolume_complete.pkl'), 'rb') as INPT:
 
 with open('df_mombud_filtered.pkl', 'rb') as inpt:
     df = pickle.load(inpt)
-
 newlabels = {'YPR': u'WT_YPR', 'YPL': u'WT_YPL', 'YPE': u'WT_YPE',
              'WT': u'WT_YPE', 'NUM1': u'ΔNUM1', 'MFB1': u'ΔMFB1',
              'YPT11': u'ΔYPT11'}
@@ -101,19 +102,22 @@ num1 = (((volr3.bud_diameter < 2) | (volr3.bud_diameter > 4.3)) &
         (volr3.media_new == u'ΔNUM1'))
 volr3 = volr3.loc[~num1]
 
+romans = ['I', 'II', 'III', 'IV', 'V']
 dfmerge = dfmom.merge(dfbud, on='name', suffixes=['_mom', '_bud'])
-dfmerge.rename(columns={0.5: 1.5,
-                        u'1.0_bud': 2.0,
-                        u'1.0_mom': 1.0},
+dfmerge.rename(columns={0.33: 'I',
+                        0.67: 'II',
+                        0.5: 'IV',
+                        u'1.0_bud': 'V',
+                        u'1.0_mom': 'III'},
                inplace=True)
 dfmerge = dfmerge.loc[dfmerge.name.isin(volr3.index)]
 
 meds = dfmerge.loc[:,
-                   dfmerge.columns.isin([0.33, 0.67, 1.0, 1.5, 2.0,
+                   dfmerge.columns.isin(['I', 'II', 'III', 'IV', 'V',
                                          u'media_new_bud', u'binvol_bud'])]
 
 alls = dfmerge.loc[:,
-                   dfmerge.columns.isin([0.33, 0.67, 1.0, 1.5, 2.0,
+                   dfmerge.columns.isin(['I', 'II', 'III', 'IV', 'V',
                                          u'media__new_bud', u'binvol_bud'])]
 # =============================================================================
 # Data long form
@@ -126,73 +130,74 @@ relabels = {'DY_median_mom': u'ΔΨ Mom', 'DY_median_bud': u'ΔΨ Bud'}
 mb_dy = pd.melt(df, id_vars=['media_new'],
                 value_name=u'ΔΨ scaled',
                 value_vars=mombud_dy_vars)
-mb_dy.rename({'variable': relabels}, inplace=True)
-
+mb_dy.replace({'variable': relabels}, inplace=True)
 size = df.loc[:, ['media_new', 'bud_diameter', 'bud_dy_var']]
 
 outkws1 = dict(default_ylims=[0.05, 0.95], plt_type='boxplot',
-               labeller=labelNormal, col_order=COL_ODRfra)
-
+               labeller=labelNormal, col_order=plt_labs[3:] + plt_labs[:3])
 outkws2 = dict(default_ylims=[0.1, 0.9],
-               labeller=labelFacet, col_order=COL_ODR)
-
-outkws3 = dict(default_ylims=[0.05, 0.95], plt_type='boxplot',
-               labeller=labelNormal, hue_order=HUE_ODR)
-
+               labeller=labelFacet)
 # =============================================================================
-#  test of sizes
-# =============================================================================
-callout = ['YPL_052315_025_RFPstack_036',
-           'YPL_042515_021_RFPstack_029',
-           'YPE_042715_013_RFPstack_048',
-           'YPE_042715_007_RFPstack_033',
-           'YPT11_071016_025_RFPstack_147',
-           'YPT11_071016_005_RFPstack_127',
-           'YPT11_071016_029_RFPstack_150']
-volr2['mark'] = volr2.loc[volr2.index.intersection(callout)].budvolratio
-with sns.color_palette('colorblind'):
-    h = sns.FacetGrid(volr2, col='media_new',
-                      col_wrap=3, col_order=HUE_ODR[:], hue='media_new',
-                      size=3, aspect=1.5)
-    for i in h.axes.flat:
-        i.axvline(x=cutoff, linewidth=1, color='m')
-    h.map(plt.scatter, 'bud_diameter', 'budvolratio')
-    h.map(plt.scatter, 'bud_diameter', 'mark', s=40, color='r', marker='v')
-    h.set(ylim=[0, 1.1])
-    plt.savefig(op.join(savefolder, 'budratio_bud_diam.png'))
-
-# =============================================================================
-# mombud ΔΨ plot
-# =============================================================================
-with sns.plotting_context('talk', font_scale=1.5):
-    plt.rcParams['figure.figsize'] = (16, 11)
-    set1 = dict(x='media_new', y=u'ΔΨ scaled',
-                hue='variable', group_key='media_new',
-                title=u'ΔΨ mother vs bud', ylim=(0, 1.), notch=True,
-                )
-
-    plv1 = plviol(**outkws1)
-    plv1.plt(data=mb_dy, **set1)
-    plv1.ax.xaxis.label.set_visible(False)
-    plv1.ax.get_legend().set_title('')
-    plv1.save_figure(op.join(savefolder, 'boxplot mombud dy.svg'))
-
+## OPTIONAL PLOTS
+#
+##  test of sizes
+#callout = ['YPL_052315_025_RFPstack_036',
+#           'YPL_042515_021_RFPstack_029',
+#           'YPE_042715_013_RFPstack_048',
+#           'YPE_042715_007_RFPstack_033',
+#           'YPT11_071016_025_RFPstack_147',
+#           'YPT11_071016_005_RFPstack_127',
+#           'YPT11_071016_029_RFPstack_150']
+#volr2['mark'] = volr2.loc[volr2.index.intersection(callout)].budvolratio
+#vlabels = dict(zip(sorted(df['media_new'].unique()), plt_labs))
+#volr2['media'] = volr2.media_new.map(vlabels)
+#with sns.color_palette('colorblind'):
+#    h = sns.FacetGrid(volr2, col='media_new',
+#                      col_wrap=3, col_order=sorted(vlabels.keys()),
+#                      hue='media',
+#                      size=3, aspect=1.5)
+#    for i in h.axes.flat:
+#        i.axvline(x=cutoff, linewidth=1, color='m')
+#    h.map(plt.scatter, 'bud_diameter', 'budvolratio')
+#    h.map(plt.scatter, 'bud_diameter', 'mark', s=40, color='r', marker='v')
+#    h.set(ylim=[0, 1.1])
+#    plt.savefig(op.join(savefolder, 'budratio_bud_diam.png'))
+#
+#mb_dy['media'] = mb_dy.media_new.map(vlabels)
+#
+## mombud ΔΨ plot
+#with sns.plotting_context('talk', font_scale=1.5):
+#    plt.rcParams['figure.figsize'] = (16, 11)
+#    set1 = dict(x='media', y=u'ΔΨ scaled',
+#                hue='variable', group_key='media',
+#                title=u'ΔΨ mother vs bud', ylim=(0, 1.), notch=True,
+#                )
+#
+#    plv1 = plviol(**outkws1)
+#    plv1.plt(data=mb_dy, **set1)
+#    plv1.ax.xaxis.label.set_visible(False)
+#    plv1.ax.get_legend().set_title('')
+#    plv1.save_figure(op.join(savefolder, 'boxplot mombud dy.svg'))
 # =============================================================================
 # frac ΔΨ plot
 # =============================================================================
+# give new labels for plotting
+labels = dict(zip(sorted(df['media_new'].unique())[:3], plt_labs[:3]))
+frac.replace({'media_new': labels}, inplace=True)
+newpal = dict(zip(plt_labs, colorblind[5:] + colorblind[:5]))
 with sns.plotting_context('talk', font_scale=1.5):
-    plt.rcParams['figure.figsize'] = (16, 11)
-    set2v = dict(x='media_new', y=u'ΔΨ scaled',
-                 group_key='media_new', inner=None, notch=True,
-                 title=u'',
-                 ylim=[0, 2.5])
+        plt.rcParams['figure.figsize'] = (16, 11)
+        set2v = dict(x='media_new', y=u'ΔΨ scaled',
+                     palette=newpal, group_key='media_new',
+                     inner=None, notch=True, title=u'',
+                     ylim=[0, 2.5])
 
-    plv2 = plviol(**outkws1)
-    plv2.plt(data=frac, **set2v)
-    plv2.ax.axhline(1.0)
-    plv2.ax.xaxis.label.set_visible(False)
-    plv2.turn_off_legend()
-    plv2.save_figure(op.join(savefolder, 'boxplot frac dy.svg'))
+        plv2 = plviol(**outkws1)
+        plv2.plt(data=frac, **set2v)
+        plv2.ax.axhline(1.0)
+        plv2.ax.xaxis.label.set_visible(False)
+        plv2.turn_off_legend()
+        plv2.save_figure(op.join(savefolder, 'boxplot frac dy.svg'))
 
 # =============================================================================
 # mom, bud,  cell axis long form DF
@@ -221,54 +226,58 @@ allsizes = pd.melt(alls,
                    value_name=u'ΔΨ scaled',
                    value_vars=meds.columns[:5].values.tolist())
 
+buddy_meds.replace({'media_new_bud': labels}, inplace=True)
 # =============================================================================
 # ranksum test for frac ΔΨ and ΔΨ along cell axis
 # =============================================================================
 # frac ΔΨ
-t1 = (frac.groupby('media_new').get_group(u'WT_YPE')[u'ΔΨ scaled'].dropna())
+t1 = (frac.groupby('media_new').get_group(u'Eth. + Glyc.')[u'ΔΨ scaled']
+      .dropna())
+cols = sorted(buddy_meds.groupby('media_new_bud').groups.keys())
 print u'frac ΔΨ YPE ranksum tests'
 print '{}'.format('*'*79)
-for t in sorted(COL_ODR[1:]):
+for t in sorted(cols[1:]):
     t2 = (frac.groupby('media_new').get_group(t)[u'ΔΨ scaled'].dropna())
     _, p = ss.ranksums(t1, t2)
     print u'p-val vs {:6s} {:6.4f}'.format(t, p)
 
 # ΔΨ along cell axis
-for t in sorted(COL_ODR[1:]):
-    print u'\nΔΨ WT_YPE against {} along cell axis'.format(t)
+for t in cols[1:]:
+    print u'\nΔΨ Eth. + Glyc. against {} along cell axis'.format(t)
     print '{}'.format('*'*79)
-    for i in [0.33, 0.67, 1.0, 1.5, 2.0]:
+    for i in ['I', 'II', 'III', 'IV', 'V']:
         t1 = (buddy_meds.groupby(['media_new_bud', 'cell axis position'])
-              .get_group((u'WT_YPE', i))[u'ΔΨ scaled'].dropna())
+              .get_group((u'Eth. + Glyc.', i))[u'ΔΨ scaled'].dropna())
         t2 = (buddy_meds.groupby(['media_new_bud', 'cell axis position'])
               .get_group((t, i))[u'ΔΨ scaled'].dropna())
         _, p = ss.ranksums(t1, t2)
-        print 'p-val at pos {:4.2f} : {:6.4f}'.format(i, p)
+        print 'p-val at pos {:3s} : {:6.4f}'.format(i, p)
 
 # =============================================================================
 # PLOTS FACETTED
 # =============================================================================
-buds_ype = buddy_meds.media_new_bud == u'WT_YPE'
-
+buds_ype = buddy_meds.media_new_bud == u'Eth. + Glyc.'
 with sns.plotting_context('talk', font_scale=1.25):
-    with sns.color_palette('colorblind'):
-        plt.rcParams['figure.figsize'] = (16, 11)
+    plt.rcParams['figure.figsize'] = (16, 11)
 
-        set7 = dict(col_wrap=3, col='media_new_bud', hue='media_new_bud',
-                    hue_order=HUE_ODR, size=4, aspect=1.2,
-                    sharex=True, sharey=True, col_order=COL_ODR[:],
-                    ylim=(0.0, 1.0))
+    set7 = dict(col_wrap=3, col='media_new_bud', hue='media_new_bud',
+                hue_order=cols[1:],
+                size=4, aspect=1.2,
+                palette=newpal,
+                sharex=True, sharey=True, col_order=cols,
+                ylim=(0.0, 1.0))
 
-        plv7 = plfacet(plt_type='pointplot', **outkws2)
-        plv7.plt(data=buddy_meds,
-                 mapargs=['cell axis position', u'ΔΨ scaled'],
-                 **set7)
-        for ax in plv7.facet_obj.axes.flat:
-            wt = sns.pointplot('cell axis position', u'ΔΨ scaled',
-                               data=buddy_meds.loc[buds_ype],
-                               ax=ax, markers='x')
-            [j.set_alpha(.5) for j in ax.collections]
-            [j.set_alpha(.35) for j in ax.lines]
+    plv7 = plfacet(plt_type='pointplot', **outkws2)
+    plv7.plt(data=buddy_meds,
+             mapargs=['cell axis position', u'ΔΨ scaled'],
+             **set7)
+    for ax in plv7.facet_obj.axes.flat:
+        wt = sns.pointplot('cell axis position', u'ΔΨ scaled',
+                           data=buddy_meds.loc[buds_ype],
+                           color=newpal[u'Eth. + Glyc.'],
+                           ax=ax, markers='x')
+        [j.set_alpha(.5) for j in ax.collections]
+        [j.set_alpha(.5) for j in ax.lines]
 
-        plv7.facet_obj.set(ylabel='', xlabel='')
-        plv7.save_figure(op.join(savefolder, 'medbuds.svg'))
+    plv7.facet_obj.set(ylabel='', xlabel='')
+    plv7.save_figure(op.join(savefolder, 'medbuds.svg'))
